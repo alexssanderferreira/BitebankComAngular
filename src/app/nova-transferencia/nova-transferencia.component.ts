05060c61d00c34c6456dcfe7b48904ce3399e205
Alexssander <alexssanderferreira@hotmail.com>
2022-07-31 15:58:25 -0300

    curso 1 com angular

---
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,32 @@
+import { style } from "@angular/animations";
+import { Component, EventEmitter, Output } from "@angular/core";
+import { Router } from "@angular/router";
+import { Transferencia } from "../models/transferenciaa.model";
+import { TransferenciaService } from "../services/transferencia.service";
+
+@Component({
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss']
+})
+export class NovaTransferenciaComponent {
+
+  @Output() aoTransferir = new EventEmitter<any>();
+
+  valor: number;
+  destino: number;
+
+  constructor(private service: TransferenciaService, private router: Router) {
+
+  }
+
+  transferir() {
+    const valorEmitir: Transferencia = { valor: this.valor, destino: this.destino }
+
+    if (valorEmitir.valor > 0 && valorEmitir.destino != undefined) {
+      this.service.adicionar(valorEmitir).subscribe(resultado => {
+        this.router.navigateByUrl('extrato')
+      }, error => console.error(error))
+    }
+  }
+}
